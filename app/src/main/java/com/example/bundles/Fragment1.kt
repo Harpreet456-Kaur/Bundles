a49339ffd9da836cbcb7cbbb1b0c186f5d1175cf
Harpreet456-Kaur <125293917+Harpreet456-Kaur@users.noreply.github.com>
2023-10-06 16:31:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bundles/Fragment1.kt
@@ -0,0 +1,76 @@
+package com.example.bundles
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bundles.databinding.Fragment1Binding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Fragment1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Fragment1 : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    lateinit var binding: Fragment1Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = Fragment1Binding.inflate(layoutInflater)
+
+        binding.btn.setOnClickListener {
+
+            val text1 = binding.edit1.text.toString()
+            val text2 = binding.edit2.text.toString()
+
+            val bundle = Bundle()
+            bundle.putSerializable("T1",text1)
+            bundle.putSerializable("T2",text2)
+
+            findNavController().navigate(R.id.action_fragment1_to_fragment22,bundle)
+        }
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Fragment1.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Fragment1().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
